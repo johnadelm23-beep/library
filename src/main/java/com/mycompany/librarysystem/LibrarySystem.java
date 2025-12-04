f1ea41def5b1bd8e93df44b7233d529055a692a0
john <johnadelm23@gmail.com>
2025-12-04 20:42:04 +0200

    Initial commit

--- src/main/java/com/mycompany/librarysystem/LibrarySystem.java
+++ src/main/java/com/mycompany/librarysystem/LibrarySystem.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.librarysystem;
 
-/**
- *
- * @author Feby Adel
- */
+
 public class LibrarySystem {
 
     public static void main(String[] args) {
